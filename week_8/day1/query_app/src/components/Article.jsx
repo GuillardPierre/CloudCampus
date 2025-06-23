9de5590d55f121630028206799c0e9fdd105b3f9
Pierre Guillard <pguillard95@gmail.com>
2025-06-23 16:43:29 +0200

    exercice 6 done

--- week_8/day1/query_app/src/components/Article.jsx
+++ week_8/day1/query_app/src/components/Article.jsx
@@ -13,6 +13,17 @@ export default function Article({ article }) {
     alert(`Le produit avec l'id ${data.id} a été modifié`);
   };
 
+  const changeProductPrice = async (id) => {
+    const product = { title: 'Updated Product', price: 39.99 };
+    const rep = await fetch(`https://fakestoreapi.com/products/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(product),
+    });
+    const data = await rep.json();
+    alert(`Le prix du produit avec l'id ${data.id} a été modifié`);
+  };
+
   return (
     <Card className='h-100'>
       <Card.Img
@@ -25,13 +36,20 @@ export default function Article({ article }) {
         <Card.Text>{article.description}</Card.Text>
         <Card.Text>{article.price} €</Card.Text>
       </Card.Body>
-      <Card.Footer>
+      <Card.Footer className='gap-1'>
         <Button
           variant='primary'
           className='mt-auto'
           onClick={() => changeProduct(article.id)}
         >
           Modifier un produit complet
+        </Button>
+        <Button
+          variant='primary'
+          className='mt-auto'
+          onClick={() => changeProductPrice(article.id)}
+        >
+          Modifer le prix
         </Button>
       </Card.Footer>
     </Card>
