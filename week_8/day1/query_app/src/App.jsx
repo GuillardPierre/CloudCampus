42b371ed0ae51c837c176733ce2b152f02a87a04
Pierre Guillard <pguillard95@gmail.com>
2025-06-23 15:22:29 +0200

    exercice 4 done

--- week_8/day1/query_app/src/App.jsx
+++ week_8/day1/query_app/src/App.jsx
@@ -5,6 +5,7 @@ import Article from './components/Article';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   const [articles, setArticles] = useState([]);
@@ -21,10 +22,27 @@ function App() {
     getData();
   }, []);
 
+  const postProduct = async () => {
+    const product = { title: 'New Product', price: 29.99 };
+    const rep = await fetch('https://fakestoreapi.com/products', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(product),
+    });
+    const data = await rep.json();
+    alert(`Le produit avec l'id ${data.id} a été créé`);
+  };
+
   return (
     <>
       <main>
-        <Container>
+        <Container className='my-5'>
+          <div className='d-flex justify-content-center mb-4'>
+            <Button variant='primary' size='lg' onClick={postProduct}>
+              <i className='bi bi-plus-lg me-2'></i> Ajouter un produit
+            </Button>
+          </div>
+
           <Row className='gy-4'>
             {articles.map((article) => (
               <Col key={article.id} xs={12} sm={6} md={3}>
