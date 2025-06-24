843c31ab22553e98c9385de391233ab5c3f4925b
Pierre Guillard <pguillard95@gmail.com>
2025-06-24 16:50:46 +0200

    add React Hook Form

--- week_8/day2/todolist/src/App.jsx
+++ week_8/day2/todolist/src/App.jsx
@@ -1,42 +1,49 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './styles/app.scss';
+import { useForm } from 'react-hook-form';
 
 function App() {
-  const [formData, setFormData] = useState({
-    name: '',
-    date: '',
-    priority: 'Low',
-    isCompleted: false,
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: 'onBlur',
+    defaultValues: {
+      name: '',
+      date: '',
+      priority: 'Low',
+      isCompleted: false,
+    },
   });
 
-  const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    setFormData({
-      ...formData,
-      [name]: type === 'checkbox' ? checked : value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   return (
     <>
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className='mb-3' controlId='name'>
           <Form.Label>Nom</Form.Label>
           <Form.Control
             type='text'
             name='name'
-            value={formData.name}
-            onChange={handleChange}
-            required
+            {...register('name', {
+              required: 'Le nom est requis',
+              minLength: {
+                value: 3,
+                message: 'Le nom doit faire au minimum 3 lettres',
+              },
+            })}
+            isInvalid={!!errors.name}
           />
+          <Form.Control.Feedback type='invalid'>
+            {errors.name?.message}
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className='mb-3' controlId='date'>
@@ -44,19 +51,19 @@ function App() {
           <Form.Control
             type='date'
             name='date'
-            value={formData.date}
-            onChange={handleChange}
-            required
+            {...register('date', {
+              required: 'La date est requise',
+            })}
+            isInvalid={!!errors.date}
           />
+          <Form.Control.Feedback type='invalid'>
+            {errors.date?.message}
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className='mb-3' controlId='priority'>
           <Form.Label>Priorité</Form.Label>
-          <Form.Select
-            name='priority'
-            value={formData.priority}
-            onChange={handleChange}
-          >
+          <Form.Select name='priority' {...register('priority')}>
             <option value='Low'>Basse</option>
             <option value='Middle'>Moyenne</option>
             <option value='High'>Élevée</option>
@@ -68,8 +75,7 @@ function App() {
             type='checkbox'
             name='isCompleted'
             label='Completé'
-            checked={formData.isCompleted}
-            onChange={handleChange}
+            {...register('isCompleted')}
           />
         </Form.Group>
 
