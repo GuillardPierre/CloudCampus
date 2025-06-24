83c461edfdf67e87a33a2f3e2523350275b1fc99
Pierre Guillard <pguillard95@gmail.com>
2025-06-24 14:50:38 +0200

    complete todolist in console

---
+++ week_8/day2/todolist/src/App.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import './styles/app.scss';
+
+function App() {
+  const [formData, setFormData] = useState({
+    nom: '',
+    date: '',
+    priorite: 'Low',
+    isCompleted: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === 'checkbox' ? checked : value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className='mb-3'>
+          <Form.Label>Nom</Form.Label>
+          <Form.Control
+            type='text'
+            name='nom'
+            value={formData.nom}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className='mb-3' controlId='date'>
+          <Form.Label>Date</Form.Label>
+          <Form.Control
+            type='date'
+            name='date'
+            value={formData.date}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label>Priorité</Form.Label>
+          <Form.Select
+            name='priorite'
+            value={formData.priorite}
+            onChange={handleChange}
+          >
+            <option value='Low'>Basse</option>
+            <option value='Middle'>Moyenne</option>
+            <option value='High'>Élevée</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Check
+            type='checkbox'
+            name='isCompleted'
+            label='Completé'
+            checked={formData.isCompleted}
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Button variant='primary' type='submit'>
+          Soumettre
+        </Button>
+      </Form>
+    </>
+  );
+}
+
+export default App;
