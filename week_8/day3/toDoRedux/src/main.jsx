2c795f6b091d3fa83c3782ca63b41217f19d5812
Pierre Guillard <pguillard95@gmail.com>
2025-06-25 15:06:36 +0200

    add redux

---
+++ week_8/day3/toDoRedux/src/main.jsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './App';
+import { StrictMode } from 'react';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
