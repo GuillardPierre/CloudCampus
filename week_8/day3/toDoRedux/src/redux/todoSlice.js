b3534708cf87a54883979fa632a5cd173b24dd79
Pierre Guillard <pguillard95@gmail.com>
2025-06-26 10:06:19 +0200

    delete return value in reducers

--- week_8/day3/toDoRedux/src/redux/todoSlice.js
+++ week_8/day3/toDoRedux/src/redux/todoSlice.js
@@ -8,14 +8,12 @@ const todoSlice = createSlice({
   reducers: {
     addTodo: (state, action) => {
       state.push({ id: Date.now(), text: action.payload, completed: false });
-      return state;
     },
     toggleTodo: (state, action) => {
       const todo = state.find((todo) => todo.id === action.payload);
       if (todo) {
         todo.completed = !todo.completed;
       }
-      return state;
     },
   },
 });
